586a62642858d2df9f83e66298df3a9e6495c300
loktionov <pompilus@gmail.com>
2017-03-22 10:44:05 +0300

    Вынос нумерации строк за сортировку

--- MySQLtask/row_number.sql
+++ MySQLtask/row_number.sql
@@ -1,14 +1,22 @@
-SELECT * FROM 
+SELECT b.rn, b.cat_id, b.art_id, b.article, b.category FROM (
+SELECT     
+  @rn:=CASE WHEN a.cat_id = @cat_id THEN @rn + 1 ELSE 1 END AS rn,
+  @cat_id:=a.cat_id AS cat, 
+  a.*
+  FROM 
   
-  (
-    SELECT @rn:=CASE WHEN c.id = @cat_id THEN @rn + 1 ELSE 1 END AS rn,
-    @cat_id:=c.id cat_id,
-        a.title AS article,        
-        c.title as category 
-  FROM articles_categories ac
-  JOIN articles a ON a.id=ac.article_id
-  JOIN categories c ON c.id=ac.category_id
-  ORDER BY category, article
-  ) a
+    (
+      SELECT 
+        c.id AS cat_id,
+        a.id AS art_id,
+          a.title AS article,        
+          c.title as category 
+    FROM articles_categories ac
+    JOIN articles a ON a.id=ac.article_id
+    JOIN categories c ON c.id=ac.category_id      
+    ORDER BY c.id, article
+    ) a 
+  
+  ) b
  
- WHERE a.rn<3
+ WHERE b.rn <= 2
